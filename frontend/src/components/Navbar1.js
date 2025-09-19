8f13e828737c6fdc07a4a21fc765b6cc39ae99e5
ShreerakshaBBdegera07 <tejuacharya56.com>
2025-09-19 12:30:00 +0530

    minor changes

--- frontend/src/components/Navbar1.js
+++ frontend/src/components/Navbar1.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.jpg';
 import './Navbar1.css';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar1 = ({ isLoginPage = false, isAuthPage = false }) => {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   // Navigation Handlers
   const handleHomeClick = () => {
     navigate('/');
+    setIsOpen(false);
   };
 
   const handleLoginClick = () => {
     navigate('/LoginPage');
+    setIsOpen(false);
+  };
+  
+  const handleMenuToggle = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -21,15 +28,21 @@ const Navbar1 = ({ isLoginPage = false, isAuthPage = false }) => {
         <img src={logo} alt="Logo" />
         <span className="navbar-name">KRISHISETU</span>
       </div>
+      
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={handleMenuToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      
+      {/* Desktop Links */}
       <ul className="navbar-links">
-        {/* Always show Home button */}
         <li>
           <a href="/" className="navbar-link" onClick={handleHomeClick}>
             HOME
           </a>
         </li>
-
-        {/* Show Login button only on auth pages (login/register) */}
         {isAuthPage && (
           <li>
             <a href="/LoginPage" className="navbar-link" onClick={handleLoginClick}>
@@ -37,8 +50,6 @@ const Navbar1 = ({ isLoginPage = false, isAuthPage = false }) => {
             </a>
           </li>
         )}
-
-        {/* Hide other buttons on login page */}
         {!isLoginPage && !isAuthPage && (
           <>
             <li>
@@ -57,6 +68,42 @@ const Navbar1 = ({ isLoginPage = false, isAuthPage = false }) => {
               </a>
             </li>
           </>
+        )}
+      </ul>
+      
+      {/* Mobile Menu as a separate container */}
+      <ul className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+        <span className="close-btn" onClick={handleMenuToggle}>&times;</span>
+        <li>
+          <a href="/" className="navbar-link" onClick={handleHomeClick}>
+            HOME
+          </a>
+        </li>
+        {!isLoginPage && !isAuthPage && (
+          <>
+            <li>
+              <a href="#about" className="navbar-link" onClick={() => setIsOpen(false)}>
+                ABOUT US
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="navbar-link" onClick={() => setIsOpen(false)}>
+                CONTACT US
+              </a>
+            </li>
+            <li>
+              <a href="/LoginPage" className="navbar-link" onClick={handleLoginClick}>
+                LOG IN
+              </a>
+            </li>
+          </>
+        )}
+        {isAuthPage && (
+          <li>
+            <a href="/LoginPage" className="navbar-link" onClick={handleLoginClick}>
+              LOG IN
+            </a>
+          </li>
         )}
       </ul>
     </nav>
